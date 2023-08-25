2d8413d77d91550429c00d0a98eee6b8972a081d
amekusa <hello@amekusa.com>
2023-08-25 15:54:03 +0900

    Improve example

--- examples/members.js
+++ examples/members.js
@@ -1,67 +1,85 @@
-// The sample data to be parsed
+// Data to be parsed
 let data = `
-*** Sample Data ***
+---- Employees List ----
 Alice {
-  gender > female
-  age    > 24
+	sex > female
+	age > 24
 }
-Bill {
-  gender > male
-  age    > 32
+Bob {
+	sex > male
+	age > 32
 }
-Chase {
-  gender > male
+Charlie {
+	sex > male
 }
-Domon {
-  gender  > male
-  species > dormouse
+Dolly {
+	sex > female
+	species > sheep
 }
 `
 
-// The parser and the rule
-const csp = require('../lib')
+// Initialize the parser
+import csp from 'cs-parser'
 let parser = csp.create()
 
+// Define the 1st rule
 parser.addRule({
-  from: /(\w+) {/,
-  to:   '}',
-  init(cx, chunk, matches) {
-    // Prepare the data container
-    cx.data = {
-      name:   matches[1],
-      gender: 'unknown',
-      age:    'unknown',
-      errors: []
-    }
-  },
-  parse(cx, chunk) {
-    // Store properties
-    let matches = chunk.match(/(\w+) +> +(\w+)/)
-    if (matches) {
-      let prop  = matches[1]
-      let value = matches[2]
-      if (cx.data[prop]) cx.data[prop] = value
-      else cx.data.errors.push('Invalid Data: ' + prop)
-    }
-  },
-  fin(cx) {
-    // Print errors
-    let errors = cx.data.errors
-    if (errors.length) {
-      console.error(errors.length + ' errors in ' + cx.data.name)
-      for (let error of errors) console.error(error)
-    }
-  }
+	from: /(\w+) {/, // Starts with '(word) {'
+	to:   '}',       //   Ends with '}'
+
+	// Context initializer
+	init(cx, chunk, matches) {
+		// Prepare a data container for chunk parser
+		cx.data = {
+			type: 'employee',
+			name: matches[1], // 'word'
+			sex: 'unknown',
+			age: 'unknown',
+			errors: []
+		}
+	},
+
+	// Chunk parser
+	parse(cx, chunk) {
+		// Check if the current chunk (line) is something like: '(word1) > (word2)'
+		let matches = chunk.match(/(\w+) > +(\w+)/)
+		if (matches) {
+			let prop  = matches[1] // 'word1'
+			let value = matches[2] // 'word2'
+
+			// Validate the matches
+			if (prop in cx.data) {
+				// Store the matches to the data container
+				cx.data[prop] = value
+			} else {
+				// Error handling
+				cx.data.errors.push('invalid property: ' + prop)
+			}
+		}
+	},
+
+	// Context finalizer
+	fin(cx) {
+		// Print errors
+		let errors = cx.data.errors
+		if (errors.length) {
+			console.error(errors.length + ' errors in ' + cx.data.name)
+			for (let err of errors) {
+				console.error(err)
+			}
+		}
+	}
 })
 
-// Parse and print the results
-let cx = parser.parse(data)
+// Parse the data and print the results
+let result = parser.parse(data)
 let i = 0
-cx.traverse(each => {
-  if (!each.data.name) return
-  i++
-  console.log('\nMember #' + i + ': ' + each.data.name)
-  console.log('--------------------')
-  console.log('   Gender: ' + each.data.gender)
-  console.log('      Age: ' + each.data.age)
+result.traverse(each => {
+	if (each.data.type == 'employee') {
+		i++
+		console.log('\nEmployee #' + i + ': ' + each.data.name)
+		console.log('--------------------')
+		console.log('   Sex: ' + each.data.sex)
+		console.log('   Age: ' + each.data.age)
+	}
 })
